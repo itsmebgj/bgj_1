9f9ad29520af17af0efe2f0a992e3a7217ffee4c
Geunjae Bae <bgj3088@gmail.com>
2023-12-07 22:07:45 +0900

    add MovePanel

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,10 @@
+
+:
+
+Dinae.java,d\e\de2fb15fe144b941bff33e8be0eeb65d422ed894
+T
+$codePackage/src/MovePanel/Dinae.java,7\c\7cb71d1b66c3e705d96baefa90662ae66b96101e
+J
+codePackage/src/Dinae.java,f\6\f6e55092ea0534fa249c6576f0b6769ad1841ba1
+J
+codePackage/src/Part1.java,8\9\89455f5af8160c7f77ddc02cf633e03b4c002c55
---
+++ codePackage/src/Dinae.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Dinae extends JFrame {
+    public Dinae() {
+
+        super("창1"); //타이틀
+        JPanel jPanel = new JPanel();
+        JButton btn1 = new JButton("창 2");
+        setSize(300, 200); //창 크기 설정
+        jPanel.add(btn1);
+        add(jPanel);
+
+        Dimension frameSize = getSize();
+
+        Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((windowSize.width - frameSize.width) / 2,
+                (windowSize.height - frameSize.height) / 2); //화면 중앙에 띄우기
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        btn1.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                new Part1();
+                setVisible(false); // 창 안보이게 하기
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new Dinae();
+    }
+}
